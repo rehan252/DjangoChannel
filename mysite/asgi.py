629c6c9d9ec1c724c3639f8fa91141d3a368e101
asimaziz <asim.aziz@oneclout.com>
2021-01-14 18:50:22 +0500

    asgi deployment changes

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -6,13 +6,14 @@ from django.core.asgi import get_asgi_application
 # Fetch Django ASGI application early to ensure AppRegistry is populated
 # before importing consumers and AuthMiddlewareStack that may import ORM
 # models.
+from mysite import chat
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 django_asgi_app = get_asgi_application()
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-
-from chat.consumers import PublicChatConsumer
+import chat.routing
 
 application = ProtocolTypeRouter({
     # Django's ASGI application to handle traditional HTTP requests
@@ -20,8 +21,8 @@ application = ProtocolTypeRouter({
 
     # WebSocket chat handler
     "websocket": AuthMiddlewareStack(
-        URLRouter([
-            url(r"", PublicChatConsumer.as_asgi()),
-        ])
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
     ),
 })
